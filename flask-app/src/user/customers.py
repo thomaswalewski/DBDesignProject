a7fcda115997da08dddcfdff86d2441a8e19dd6e
Thomas Walewski <thomaswalewski5@gmail.com>
2022-12-12 09:27:45 -0500

    12/12 start

--- flask-app/src/user/customers.py
+++ flask-app/src/user/customers.py
@@ -10,10 +10,8 @@ user = Blueprint('user', __name__)
 @user.route('/user/<user_id>/plants', methods=['GET'])
 def get_plants(user_id):
     cursor = db.get_db().cursor()
-    cursor.execute('select species, height, p.id, plant_date, g2.name as garden_name, g2.id as garden_id'
-                   ' from plants p JOIN garden g2 on g2.id = p.garden_id'
-                   ' where garden_id = (select g.id from garden g JOIN user u on u.id = '
-                   'g.user_id where g.user_id = {0})'.format(user_id))
+    cursor.execute('select p.species, p.height, p.plant_date, g.name as Garden_Name, p.id as plant_id '
+                   ' from garden g JOIN plants p on g.id = p.garden_id where g.user_id = {0}'.format(user_id))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -42,17 +40,54 @@ def get_watering(plant_id):
 
 @user.route('/<user_id>/addPlant', methods=['POST'])
 def add_plant(user_id):
-
     cursor = db.get_db().cursor()
 
     species1 = request.form['species']
     plant_date1 = request.form['plant_date']
     height1 = request.form['height']
     garden_id1 = request.form['garden_id']
+    bed_num = request.form['bed_num']
+    rec_wf = request.form['rec_wf']
+    rec_wa = request.form['rec_wa']
 
-    cursor.execute('Insert into plants(species, garden_id, plant_date, height) values (%s, %s, %s, %s)',
-                   (species1, garden_id1, plant_date1, height1))
+    cursor.execute('Insert into plants(species, garden_id, plant_date, height, bed_num, rec_water_freq, rec_water_amount)'
+                   ' values (%s, %s, %s, %s, %s, %s, %s)',
+                   (species1, garden_id1, plant_date1, height1, bed_num, rec_wf, rec_wa))
     cursor.connection.commit()
     cursor.close()
     get_plants(user_id)
     return 'success'
+
+
+@user.route('/<user_id>/gardens', methods=['GET'])
+def get_gardens(user_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select id as value, name as label from garden where user_id = {0}'.format(user_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+
+@user.route('/<user_id>/<plant_id>', methods=['GET'])
+def get_plant(user_id, plant_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from plants where id = {0}'.format(plant_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+
